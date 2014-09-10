2018d266c046a2758901c4391447a18420a574e8
BorysHaltsov <borishaltsov92@mail.ru>
2014-09-10 16:15:01 +0300

    Card random generator.

---
+++ Cards/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cards
+{
+    class Card
+    {
+        public Suits Suit { get; set; }
+        public Values Value { get; set; }  
+
+        public Card(Suits suit, Values value)
+        {
+            Suit = suit;
+            Value = value; 
+        }
+
+        public string Name
+        {
+            get { return Value + " of " + Suit;}
+        } 
+    }
+}
---
+++ Cards/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cards
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+        }
+
+        readonly Random _random = new Random(); 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var card = new Card((Suits) _random.Next(4), (Values) _random.Next(1,14)); 
+            MessageBox.Show(card.Name); 
+        }
+    }
+}
